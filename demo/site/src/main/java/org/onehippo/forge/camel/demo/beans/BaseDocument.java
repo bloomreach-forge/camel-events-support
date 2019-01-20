7808e64e5a7a32c137420fe32f445ebffa17a7ca
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-20 15:48:26 +0900

    HIPFORGE-249: removing old demo

--- demo/site/src/main/java/org/onehippo/forge/camel/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.camel.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="camelhippoevtdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
