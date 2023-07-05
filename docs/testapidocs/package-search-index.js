6d199b32a8c60691261d2b62eedc3bf7abad0f89
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2023-07-05 13:25:17 +0300

    HIPFORGE-439 : Regenerate docs on master

---
+++ docs/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.onehippo.forge.camel.component.hippo"}]
---
+++ docs/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.onehippo.forge.camel.component.hippo","l":"HippoEventConsumerTest"},{"p":"org.onehippo.forge.camel.component.hippo","l":"HippoEventConverterTest"}]
