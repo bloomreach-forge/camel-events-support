0099a2ca0a19ca1dd8ac73b183700a738dd421e2
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2024-10-04 14:14:19 +0200

    FORGE-534 : regenerate docs on master

--- docs/testapidocs/package-search-index.js
+++ docs/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.onehippo.forge.camel.component.hippo"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.onehippo.forge.camel.component.hippo"}];updateSearchResults();
